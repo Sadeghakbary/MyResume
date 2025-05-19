bd3c4c6b083a871e6c17c464446e3e1c0cdfe549
sadegh <sadeghakbary1381@gmail.com>
2025-05-19 19:12:51 +0330

    last part

--- src/componants/navbar.jsx
+++ src/componants/navbar.jsx
@@ -1,12 +1,18 @@
 import GitHubIcon from "@mui/icons-material/GitHub";
 import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
-import { AppBar, Toolbar, Typography, Box, Avatar } from "@mui/material";
+import { AppBar, Toolbar, Typography, Box, Avatar, Button } from "@mui/material";
 
 export default function Navbars() {
+
+  const buttonClickHandler = () => {
+    console.log('shod');
+
+  }
   return (
     <AppBar position="static" color="primary">
       <Toolbar sx={{ display: "flex", justifyContent: "flex-start", alignItems: "center" }}>
+
         <Box
           sx={{
             display: "flex",
@@ -17,16 +23,14 @@ export default function Navbars() {
         >
           <Avatar
             alt="Logo"
-            src="src/assets/OIP.jpg"
-            sx={{ width: 100, height: 100 , marginTop : '1rem'}}
+            src="src/assets/profile.jpg"
+            sx={{ width: 100, height: 100, marginTop: '1rem' }}
           />
-          <div style={{display:"flex" , justifyContent:"space-between"}}>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
             <GitHubIcon sx={{ color: "white", marginTop: 1 }} />
             <InstagramIcon sx={{ color: "white", marginTop: 1 }} />
             <YouTubeIcon sx={{ color: "white", marginTop: 1 }} />
           </div>
-
-
         </Box>
 
         <Box sx={{ display: "flex", flexDirection: "column", color: "white" }}>
@@ -37,6 +41,10 @@ export default function Navbars() {
             React Developer
           </Typography>
         </Box>
+
+        <Button onClick={() => buttonClickHandler} variant="contained" color="secondary" sx={{ marginLeft: '70rem' }}>
+          we can change the world
+        </Button>
       </Toolbar>
     </AppBar>
   );
